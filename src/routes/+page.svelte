96496e7e8fb863396a1642aad7268d857b492f1e
Joseph Lloyd <joseph.lloyd17@hotmail.com>
2023-12-28 16:54:24 +0000

    daily changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<main>
+  <h1>Joseph Lloyd</h1>
+  <section class="links">
+    <div class="link">
+      <a href="#">Blog</a>
+    </div>
+    <div class="link">
+      <a href="#">Photos</a>
+    </div>
+    <div class="link">
+      <a href="https://www.linkedin.com/in/josephm-lloyd/">LinkedIn</a>
+    </div>
+    <div class="link">
+      <a href="#">CV</a>
+    </div>
+    <div class="link">
+      <a href="#">Code</a>
+    </div>
+  </section>
+</main>
+
+<style>
+  main {
+    width: 900px;
+    height: 100vh;
+    background-color: #001D3D;
+  }
+
+  .links {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .link {
+    margin-bottom: 20px;
+  }
+
+  .link a {
+    font-size: x-large;
+  }
+
+  
+</style>
